53dc44fc425aa33d09672c9ec56bd778bc119abd
EmilyLow <emlow@me.com>
2021-05-02 14:50:10 -0500

    Added basic knex structure

---
+++ src/models/events.js
@@ -0,0 +1,6 @@
+const db = require("../data/db-config");
+
+//This is where you make intermediary methods to interact with database
+
+
+// module.exports = {intermediary methods}
--- src/routes/events.js
+++ src/routes/events.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+//Import Users from models, and use to access database
+//The try and catch stuff belongs here
+
 router.get('/', function(req, res, next) {
     res.send("events get");
 });
